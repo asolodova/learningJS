05ba8fcaf519d2e55ebccf9dae1b0c15e27f71e6
asolodova <angel.solodova@gmail.com>
2020-02-02 16:16:12 +0300

    Added delete cart and clear all cart
    Signed-off-by: asolodova <angel.solodova@gmail.com>

--- shoppingcart/js/app.js
+++ shoppingcart/js/app.js
@@ -1,5 +1,7 @@
 // Variables
-const courses = document.querySelector('#courses-list');
+const courses = document.querySelector('#courses-list'),
+    shoppingCartContent = document.querySelector('#cart-content tbody'),
+    clearCartBtn = document.querySelector('#clear-cart');
 
 
 
@@ -9,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
     // When a new course is added
     courses.addEventListener('click', buyCourse);
+
+    // when the remove button is clicked
+    shoppingCartContent.addEventListener('click', removeCourse)
+
+    // when the clear cart
+    clearCartBtn.addEventListener('click', clearCart);
 
 }
 
@@ -26,4 +34,47 @@ function buyCourse(e) {
 }
 function getCourseInfo(course) {
     console.log(course);
+    // Create an object with course data
+    const courseInfo = {
+        image: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.price span').textContent,
+        id: course.querySelector('a').getAttribute('data-id')
+    }
+    console.log(courseInfo.image);
+    // Insert info the shopping cart
+    addIntoCart(courseInfo);
+}
+function addIntoCart(course) {
+    // Create a <tr>
+    const row = document.createElement('tr');
+
+    // Build the template
+    row.innerHTML = `
+        <tr>
+            <td>
+                <img src="${course.imag}">
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>            
+
+    `;
+
+    shoppingCartContent.appendChild(row);
+}
+// remove course
+function removeCourse(e) {
+    if(e.target.classList.contains('remove')){
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearCart() {
+    // shoppingCartContent.innerHTML = '';
+    while (shoppingCartContent.firstChild){
+        shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+    }
 }
